4955b36c78130191faed4bee0c515d1cd79139c7
RizkyAngga193040160 <193040160@mail.unpas.ac.id>
2021-12-15 20:33:15 +0700

    Update fungsi Delete dan View

---
+++ resources/views/dashboard/barang/view.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="container-fluid">
+    <div class="d-flex justify-content-center">
+    <a href="/dashboard/barang/{{$barang->slug}}/edit" class="btn btn-warning m-2"><i class="bi bi-pencil"></i> Edit</a>
+    <form action="/dashboard/barang/{{$barang->slug}}" method="post" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger m-2" onclick="return confirm('Yakin ingin menghapus {{$barang->nama_barang}}?')"><i class="bi bi-trash"></i> Hapus</button>
+    </form>
+    </div>
+</div>
+    <div class="container-fluid">
+        <div class="row mb-5 justify-content-center">
+            <div class="col-sm-4">
+                <img src="..." alt="..." width="320px" class="mt-4 mb-4">
+            </div>
+            <div class="col-md-5">
+                <div class="card border-white mb-3" style="max-width: 30rem;">
+                <div class="card-body">
+                    <h3 class="card-title">{{$barang->nama_barang}}</h3>
+                    <p class="fw-bold">Terjual <span class="fw-normal">10</span></p>
+                    <h1 class="card-text mb-3"> 
+                        @money($barang->harga)
+                    </h1>
+                    <p class="card-text fw-bold mb-1">Rincian</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                        <p class="card-text">Kategori :  <a href="" class="text-decoration-none">{{$barang->category->nama}}</a></p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="card-text">
+                                Support :  {{$barang->socket}} - {{$barang->ram_support}}
+                            </p></li>
+                        <li class="list-group-item"><p class="card-text">Tersedia :  {{$barang->stok}} unit</p></li>
+                    </ul>
+                    <p class="card-text mt-3">{{$barang->desc}}</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
